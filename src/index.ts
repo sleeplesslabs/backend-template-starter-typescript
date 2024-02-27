12e2aafb2826ff4d26d8f0b088d6d5a9dba427f9
Farhan Rivaldy <fariv.fariv12@gmail.com>
2024-02-28 01:07:47 +0800

    chore: init folder and add logger helpers

---
+++ src/index.ts
@@ -0,0 +1,23 @@
+import express from 'express';
+import { logger } from './helpers/log';
+
+
+try {
+    const app = express();
+    app.use(express.json());
+    app.use(express.urlencoded({extended: true}));
+
+    app.get("/", (req, res) => {
+        res.send('<img style="display: block; margin: auto;" src="https://cdn.epicstream.com/images/ncavvykf/epicstream/a54b9c16f0f9e2de831b32febc169e734e4ded3d-1920x1080.png?rect=0,36,1920,1008&w=1200&h=630&auto=format"/>')
+    });
+
+    const port = 3000;
+
+
+    app.listen(port, () => {
+        logger.info("apps running on port " + port);
+    });    
+} catch (error: any) {
+    logger.error("failed to running apps, error : " + error.message);
+    process.exit(1);
+}
