d0498a811a2ffea3263f203c1bfb15beeefefeef
HaradaKumiko <fariv.fariv12@gmail.com>
2024-03-07 17:53:27 +0800

    better logging

--- src/index.ts
+++ src/index.ts
@@ -24,7 +24,7 @@ try {
     app.use(function (req, res) {
         logger.info("Invalid Request From: " + req.url + " Using HTTP Method: " + req.method)
         res.status(404).send('<img style="display: block; margin: auto;" src="https://cdn.epicstream.com/images/ncavvykf/epicstream/a54b9c16f0f9e2de831b32febc169e734e4ded3d-1920x1080.png?rect=0,36,1920,1008&w=1200&h=630&auto=format"/>');
-      })
+    });
     app.listen(port, () => {
         logger.info("apps running on port " + port);
     });    
